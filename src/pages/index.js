4917cfea3db4838ce9e12e3e7bab6f73e0569e65
Fajrian Aidil Pratama <fajrianaidilp@gmail.com>
2023-10-01 17:07:16 +0800

    fix(meta): fix website meta tag description

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="Streamline BPS Statistics API integration in Flutter. Easily fetch, analyze, and visualize data."
     >
       <HomepageHeader />
       <main>
